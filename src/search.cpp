8a1261e6ca2d65a4af74f38c3c6db8dda3a4aaee
TheRealGioviok <64363733+TheRealGioviok@users.noreply.github.com>
2024-11-06 03:33:58 +0100

    bench 4943478

--- src/search.cpp
+++ src/search.cpp
@@ -30,9 +30,10 @@ static inline S32 reduction(Depth d, U16 m, bool isQuiet, bool isPv)
     return reductionTable[isQuiet][std::min((int)d,64)][std::min((int)m,64)] - isPv * RESOLUTION;
 }
 
-static inline Score futilityMargin(Depth depth, bool improving)
+static inline Score futilityMargin(Depth depth, S32 improvement)
 {
-    return futilityMarginDelta * (depth - improving);
+    const S32 cieck = improvement > 0 ? 1536 * improvement / (std::abs(improvement) + 64) : 0;
+    return (futilityMarginDelta * std::max(1,(depth * RESOLUTION - cieck))) / RESOLUTION;
 }
 
 static inline int sortTTUp(MoveList &ml, PackedMove ttMove)
@@ -224,7 +225,7 @@ Score Game::search(Score alpha, Score beta, Depth depth, const bool cutNode, SSt
     if (!PVNode && !excludedMove)
     {
         // RFP
-        if (depth <= RFPDepth && abs(eval) < mateScore && eval - futilityMargin(depth, improving) >= beta) // && !excludedMove)
+        if (depth <= RFPDepth && abs(eval) < mateScore && eval - futilityMargin(depth, improvement) >= beta) // && !excludedMove)
             return eval;
 
         // Null move pruning
