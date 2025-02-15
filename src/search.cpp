a39526453e7bbb3b09b50d23ea60021d8f3394c6
TheRealGioviok <425gioviok@gmail.com>
2025-02-15 18:07:23 +0100

    bench 3344304

--- src/search.cpp
+++ src/search.cpp
@@ -194,7 +194,7 @@ Score Game::search(Score alpha, Score beta, Depth depth, bool cutNode, SStack *s
     {
         // Check if the eval is stored in the TT
         rawEval = tte->eval != noScore ? tte->eval : evaluate();
-        eval = ss->staticEval = ttMove ? rawEval : correctStaticEval(pos, rawEval);
+        eval = ss->staticEval = correctStaticEval(pos, rawEval);
         // Also, we might be able to use the score as a better eval
         if (ttScore != noScore && (ttBound == hashEXACT || (ttBound == hashUPPER && ttScore < eval) || (ttBound == hashLOWER && ttScore > eval)))
             eval = ttScore;
@@ -551,7 +551,7 @@ Score Game::quiescence(Score alpha, Score beta, SStack *ss)
     }
     else if (ttHit){
         rawEval = tte->eval != noScore ? tte->eval : evaluate();
-        ss->staticEval = bestScore = ttMove ? rawEval : correctStaticEval(pos, rawEval);
+        ss->staticEval = bestScore = correctStaticEval(pos, rawEval);
         if (ttScore != noScore && (ttBound == hashEXACT || (ttBound == hashUPPER && ttScore < bestScore) || (ttBound == hashLOWER && ttScore > bestScore)))
             bestScore = ttScore;
     }
