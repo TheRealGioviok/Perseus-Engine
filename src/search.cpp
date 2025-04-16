ec8b33ce2c247cfb77363680f3da829fb335c491
TheRealGioviok <425gioviok@gmail.com>
2025-04-16 11:14:59 +0200

    bench 3084453

--- src/search.cpp
+++ src/search.cpp
@@ -214,7 +214,7 @@ Score Game::search(Score alpha, Score beta, Depth depth, bool cutNode, SStack *s
     if (!PVNode && !excludedMove)
     {
         // RFP
-        if (depth <= RFPDepth() && abs(eval) < mateValue && eval - std::max(15,futilityMargin(depth, improving) - 60*cutNode) >= beta) // && !excludedMove)
+        if (depth <= RFPDepth() && abs(eval) < mateValue && eval - std::max((Score)15,futilityMargin(depth, improving)) >= beta) // && !excludedMove)
             return eval;
         
         // Razoring
