15a922b71b2906cebf5667749d6e762597d5f802
Giovanni Maria Manduca <425gioviok@gmail.com>
2025-05-08 17:27:40 +0200

    bench 2440504

--- src/search.cpp
+++ src/search.cpp
@@ -220,7 +220,7 @@ Score Game::search(Score alpha, Score beta, Depth depth, bool cutNode, SStack *s
     if (!PVNode && !excludedMove)
     {
         // RFP
-        if (depth <= RFPDepth() && abs(eval) < mateValue && eval - std::max((Score)15,futilityMargin(depth, improving)) - (corrplexity / 3) >= beta) // && !excludedMove)
+        if (depth <= RFPDepth() && abs(eval) < mateValue && eval - std::max(15,futilityMargin(depth, improving) + (corrplexity / 3)) >= beta) // && !excludedMove)
             return eval;
         
         // Razoring
